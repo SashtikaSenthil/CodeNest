540ea81cb47c7a5e5ce746c454354bdb6c891140
Jayanthan S <144931034+jayanthansenthilkumar@users.noreply.github.com>
2025-04-28 22:14:09 +0530

    Removed Unwanted lines in this file

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for=
 app = Flask(__name__)
-
 # Route for Login Page
 @app.route('/')
 def login():
     return render_template('loginpage.html')
-
 # Route for Welcome Page
 @app.route('/welcome')
 def welcome():
     return render_template('welcomepage.html')
-
 # Route for subject Page
 @app.route('/subject')
 def subject():
     return render_template('subjectspage.html')
-
 if __name__ == "__main__":
     app.run(debug=True)
